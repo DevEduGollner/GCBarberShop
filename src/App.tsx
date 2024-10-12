fc45ff4ae01a5fb1360ee634ec4b60f32b84a140
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-12 17:39:20 -0300

    Criando HTML da HomePage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/Header";
+import HomePage from "./components/HomePage";
 
 export default function App() {
-  return <Header />;
+  return <HomePage />;
 }
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -2,6 +2,16 @@ import './styles.css';
 
 export default function HomePage() {
     return (
-        <h1>Olá</h1>
+        <section className="home-page-introduction">
+<div className="home-page-content container">
+<div className="introduction-description">
+<h3>GC Barber Shop</h3>
+<p>Está preparado para o MELHOR CORTE de sua vida junto de muita resenha? então cola na GC BARBER SHOP.</p>
+<p>Clique no botão abaixo para mais detalhes:</p>
+
+<a href="best-details.html"><button>Mais detalhes</button></a>
+</div>    
+</div>
+        </section>
     );
 }
