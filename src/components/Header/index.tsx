43749b82c6e51d69b3aaae92b5c7fea0091dd815
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-12 16:25:16 -0300

    Criando HTML do Header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,42 @@
 import './styles.css';
 
 export default function Header() {
+
     return (
         <header>
-
-        </header>
+        <nav className="nav-bar container">
+          <div className="logo">
+            <img src="assets/GCBarber-Logo.png" alt="" />
+          </div>
+  
+          <div className="list-navegation">
+            <ul>
+              <li className="nav-item"><a href="">Inicio</a></li>
+              <li className="nav-item"><a href="#sla123">Serviços</a></li>
+              <li className="nav-item"><a href="">Sobre Nós</a></li>
+              <li className="nav-item"><a href="">Fale Conosco</a></li>
+            </ul>
+          </div>
+  
+          <div className="mobile-menu-icon">
+            <button >
+              <img
+                className="icon"
+                src="assets/menu-items-white.svg"
+                alt="Abrir Opções"
+              />
+            </button>
+          </div>
+        </nav>
+  
+        <div className="mobile-menu">
+          <ul>
+            <li className="nav-item"><a href="">Inicio</a></li>
+            <li className="nav-item"><a href="">Serviços</a></li>
+            <li className="nav-item"><a href="">Sobre Nós</a></li>
+            <li className="nav-item"><a href="">Fale Conosco</a></li>
+          </ul>
+        </div>
+      </header>
     );
 }
