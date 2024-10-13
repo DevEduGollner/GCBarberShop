b3b615ab7c6353adc4fed171b90478527294553a
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-13 01:57:00 -0300

    Refatorando código, pois irei fazer o componente ServicesOptions já vir com os ServiceItem de forma especifica.

--- src/components/ServicesOptions/index.tsx
+++ src/components/ServicesOptions/index.tsx
@@ -3,7 +3,42 @@ import './styles.css';
 export default function ServicesOptions() {
     return(
         <div className="services-options">
-            
+          <div className="services-item">
+            <div className="img-services">
+              <img src="assets/corte-de-cabelo-1.jpg" alt="Corte de Cabelo" />
+            </div>
+            <div className="description-services">
+              <h3>Corte de Cabelo</h3>
+              <h4>R$ 20.00</h4>
+            </div>
+          </div>
+          <div className="services-item">
+            <div className="img-services">
+              <img src="assets/corte-de-cabelo-7.jpg" alt="Descoloração" />
+            </div>
+            <div className="description-services">
+              <h3>Descoloração</h3>
+              <h4>R$ 100.00</h4>
+            </div>
+          </div>
+          <div className="services-item">
+            <div className="img-services">
+              <img src="assets/corte-de-cabelo-4.jpg" alt="Pigmentação" />
+            </div>
+            <div className="description-services">
+              <h3>Pigmentação</h3>
+              <h4>R$ 40.00</h4>
+            </div>
+          </div>
+          <div className="services-item">
+            <div className="img-services">
+              <img src="assets/corte-de-cabelo-6.jpg" alt="Luzes/Reflexo" />
+            </div>
+            <div className="description-services">
+              <h3>Luzes/Reflexo</h3>
+              <h4>R$ 60.00</h4>
+            </div>
+          </div>
         </div>
     );
 }
