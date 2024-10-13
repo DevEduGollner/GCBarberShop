2484b5c8e021b5355054e9401efbf9e0d9e6732c
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-13 00:27:20 -0300

    Abstraindo mais os componentes, criando IntroductionDescription e Button.

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,17 +4,7 @@ export default function HomePage() {
   return (
     <section className="home-page-introduction">
       <div className="home-page-content container">
-        <div className="introduction-description">
-          <h3>GC Barber Shop</h3>
-          <p>
-            Está preparado para o MELHOR CORTE de sua vida junto de muita
-            resenha? então cola na GC BARBER SHOP.
-          </p>
-          <p>Clique no botão abaixo para mais detalhes:</p>
-          <a href="best-details.html">
-            <button>Mais detalhes</button>
-          </a>
-        </div>
+       
       </div>
     </section>
   );
---
+++ src/components/IntroductionDescription/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+export default function IntroductionDescription() {
+    return (
+        <div className="introduction-description">
+        <h3>GC Barber Shop</h3>
+        <p>
+          Está preparado para o MELHOR CORTE de sua vida junto de muita
+          resenha? então cola na GC BARBER SHOP.
+        </p>
+        <p>Clique no botão abaixo para mais detalhes:</p>
+        <a href="">
+          <button>Mais detalhes</button>
+        </a>
+      </div>
+    );
+}
