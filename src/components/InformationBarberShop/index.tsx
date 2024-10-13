db20c73cea3fdb94b3c001ee7fe09b7c77fdd291
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-13 01:19:05 -0300

    Criando HTML do componente InformationBarberShop.

--- src/components/InformationBarberShop/index.tsx
+++ src/components/InformationBarberShop/index.tsx
@@ -2,6 +2,20 @@ import './styles.css';
 
 export default function InformationBarberShop() {
     return (
-        <h1>Informations</h1>
+        <div className="informations">
+        <h2>GC Barber Shop</h2>
+        <div className="important-informations">
+          <h3>Horário de funcionamento:</h3>
+          <p>De Segunda a sábado</p>
+          <h3>Atendimento:</h3>
+          <p>Atendemos tanto por agendamento quanto por ordem de chegada</p>
+          <h3>Localização:</h3>
+          <p>Av. Prof. Fernando Duarte Rabelo, 860 - Maria Ortiz</p>
+        </div>
+        <div className="invitation">
+          <h3>Quer um corte fera com muita resenha?</h3>
+          <button>Quero</button>
+        </div>
+      </div>
     );
 }
