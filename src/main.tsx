f750375323143f38ee552ce1f969a4c841cb3f13
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-13 00:52:31 -0300

    Criando rota para HomePage aninhada ao App.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.tsx'
+import HomePage from './routes/HomePage/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
 <BrowserRouter>
 <Routes>
   <Route path="/" element={<App />}> 
+  <Route index element={<HomePage />} />
   </Route>
 </Routes>
 </BrowserRouter>
