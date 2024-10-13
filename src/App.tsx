3ac27a0a7d307961276d220602a4e676aab48958
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-13 00:15:30 -0300

    Adicionando React Router ao projeto e configurando para mexermos com rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,17 +1,21 @@
-import './styles.css';
+import "./styles.css";
 
 export default function HomePage() {
-    return (
-        <section className="home-page-introduction">
-<div className="home-page-content container">
-<div className="introduction-description">
-<h3>GC Barber Shop</h3>
-<p>Está preparado para o MELHOR CORTE de sua vida junto de muita resenha? então cola na GC BARBER SHOP.</p>
-<p>Clique no botão abaixo para mais detalhes:</p>
-
-<a href="best-details.html"><button>Mais detalhes</button></a>
-</div>    
-</div>
-        </section>
-    );
+  return (
+    <section className="home-page-introduction">
+      <div className="home-page-content container">
+        <div className="introduction-description">
+          <h3>GC Barber Shop</h3>
+          <p>
+            Está preparado para o MELHOR CORTE de sua vida junto de muita
+            resenha? então cola na GC BARBER SHOP.
+          </p>
+          <p>Clique no botão abaixo para mais detalhes:</p>
+          <a href="best-details.html">
+            <button>Mais detalhes</button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
