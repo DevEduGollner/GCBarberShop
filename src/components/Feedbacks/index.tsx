44f23fc84fcf4f564d4214b06e3c0496d8aa2088
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-14 14:35:28 -0300

    Adicionando HTML.

--- src/components/Feedbacks/index.tsx
+++ src/components/Feedbacks/index.tsx
@@ -1,7 +1,61 @@
-import './styles.css';
+import "./styles.css";
 
 export default function Feedbacks() {
-    return (
-<h1>Feedbacks</h1>
-    );
+  return (
+    <div className="container">
+      <h1>O que estão dizendo</h1>
+
+      <div className="card-feedback">
+        <p>
+          Ótimo atendimento, corta bem e por um preço super acessivel, estarei
+          de volta em outras oportunidades.
+        </p>
+        <div className="card-feedback-bottom">
+        <p>- Eduardo Gollner</p>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        </div>
+      </div>
+
+      <div className="card-feedback">
+        <p>Corte fera demais, manda muito mano!! tmjj sempre.</p>
+        <div className="card-feedback-bottom">
+        <p>- Gabriel Simas</p>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        </div>
+      </div>
+
+      <div className="card-feedback">
+        <p>O reflexo ficou bala mano, bem demais, e o atendimento também é diferenciado.</p>
+        <div className="card-feedback-bottom">
+        <p>- Josué</p>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        </div>
+      </div>
+
+      <div className="card-feedback">
+        <p>Sempre surpreende no corte irmão, meu barbeiro titular!! Recomendo muito.</p>
+        <div className="card-feedback-bottom">
+        <p>- Matheuzin</p>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        <span className="estrela">&#9733;</span>
+        </div>
+
+      </div>
+    </div>
+  );
 }
