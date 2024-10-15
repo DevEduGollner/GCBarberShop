9987cd8cece52069d35d75c14857b8f1a1b279e1
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-15 00:06:48 -0300

    Criando HTML.

--- src/components/DescriptionBarber/index.tsx
+++ src/components/DescriptionBarber/index.tsx
@@ -1,7 +1,39 @@
-import './style.css';
+import "./style.css";
 
 export default function DescriptionBarber() {
-    return (
-        <h1>Description</h1>
-    );
+  return (
+    <section className="section-description-barber">
+      <div>
+        <h1>Sobre Mim</h1>
+        <div className="description-barber">
+          <p>
+            Fala galera, tudo bem com vocês?! Queria contar para vocês um pouco
+            da minha história e o começo da barbearia “GC BARBER” Desde o início
+            do ensino médio eu tinha um grande sonho de ser jogador de futebol.
+            Por conta de algumas questões na época, eu tive que escolher se
+            investia em um curso ou continuaria buscando realizar meu sonho de
+            jogador profissional. Por fim, quando acabei o ensino médio, já não
+            pensavam mais no futebol. Mais fiquei em dúvida por qual curso
+            fazer. E eu sempre me amarrei em cortes de cabelo e lá no fundo
+            sempre pensava em como seria se envolver nessa profissão. Até que no
+            começo do ano de 2023 eu comecei a cursa o curso na área da
+            barbearia na “ESCABEL” em Serra ÉS. Aparti dali, começou um sonho e
+            um desejo de sempre trazer a melhor versão das pessoas, através de
+            um corte de cabelo. Terminando meu curso, comecei a usar a garagem
+            da minha casa para corta cabelo de amigos e conhecidos. Mais algo
+            que era pequeno, foi começando a tomar forma e se torna algo muito
+            grande e numa velocidade que nem eu esperava. Lembro que fui até o
+            ponto que hoje tenho a minha barbearia com 1000$ no bolso, para
+            começar meu próprio negócio. Aluguei o ponto e no dia 7 de abril de
+            2024 inaugurei a barbearia “GC BARBER” Desde então, já faz 7 meses
+            que estou nessa trajetória e Deus tem cuidado de cada detalhe.
+            Espero que tenham curtido um pouco da minha história, creio que é só
+            o começo de tudo que o senhor tem para minha vida na área da
+            barbearia!! Por isso, nunca desista dos seus sonhos!um
+            grande abraço!!! 🙏🏾
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
